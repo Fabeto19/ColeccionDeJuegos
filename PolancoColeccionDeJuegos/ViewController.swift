873f5046f8c0ec06c9bb37bfc2997a18be21d3ef
Fabeto19 <fabricio.polanco@tecsup.edu.pe>
2023-05-17 11:35:24 -0500

    Initial Commit

---
+++ PolancoColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PolancoColeccionDeJuegos
+//
+//  Created by Fabricio on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
